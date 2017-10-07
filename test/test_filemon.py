06db68a1d3b0cca68519c0697b3bc08287428afd
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-10-07 13:00:20 -0700

    Added test_write_sample

--- test/test_filemon.py
+++ test/test_filemon.py
@@ -1,5 +1,6 @@
 import os
 from test import TestFileGeneric
+import time
 from unittest import TestCase
 
 from filemon import FileMonitor
@@ -27,4 +28,24 @@ class TestFileMonitor(TestFileGeneric):
         with open(monitored_file, 'a+') as f:
             f.write("a")
         self.assertEqual(2, fm.get_file_size(monitored_file)[0])
+        
+    def test_write_sample(self):
+        monitored_file = os.path.join(self.tmp_folder, "file.txt")
+        self._register_tmp_file(monitored_file)
+        
+        fm = FileMonitor()
+        time_stamp = 5000
+        fm.write_sample(monitored_file,time_stamp, 10, 5.0,"{}:{}:{}")
+        fm.write_sample(monitored_file,time_stamp+2, 12, 6.0,"{}:{}:{}")
+        
+        with open(monitored_file, 'r') as f:
+            lines = f.readlines()
+            
+            self.assertEqual(lines,
+                             ["5000:10:5.0"
+                              "5002:12:6.0" ])
+            
+            
+              
+
     
