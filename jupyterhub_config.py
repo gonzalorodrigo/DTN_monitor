7bdf26f9c7f30779a9e563a46aaf8be023c441d6
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-11-05 23:50:23 -0700

    jupyter cookie location can be set with env var: JUPYTERHUB_LOCATION

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,3 +1,7 @@
+import os
+j_location=os.getenv("JUPYTERHUB_LOCATION", '/home/myuser')
+
 c = get_config()
-c.JupyterHub.cookie_secret_file = '/home/myuser/jupyterhub_cookie_secret'
-c.JupyterHub.db_url = '/home/myuser/jupyterhub.sqlite'
+c.JupyterHub.cookie_secret_file = ('{}/jupyterhub_cookie_secret'
+									''.format(j_location))
+c.JupyterHub.db_url = '{}/jupyterhub.sqlite'.format(j_location)
